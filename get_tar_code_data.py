d7d5c4c04914549512abc3e5d6de37987c793eec
shibusama <1319119251@SHIBUSAMA>
2025-05-08 00:22:44 +0800

    feat:func complete

---
+++ get_tar_code_data.py
@@ -0,0 +1,10 @@
+"""
+把数据分门别类
+"""
+import sqlite3
+conn_mydata = sqlite3.connect('mydata.db')
+
+
+
+
+
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from datetime import date
 conn_his = sqlite3.connect('his2025.db')
 conn_sys = sqlite3.connect('system.db')
 conn_tmp = sqlite3.connect('tmp.db')
+conn_mydata = sqlite3.connect('mydata.db')
 
 current_date = '20250426'
 _current_date = date.today().strftime('%Y%m%d')
@@ -14,7 +15,10 @@ _current_date = date.today().strftime('%Y%m%d')
 
 def only_letters(s):
     return ''.join(re.findall(r'[A-Za-z]+', s))
+
+
 conn_his.create_function("only_letters", 1, only_letters)
+
 
 def get_target_code(date):
     """
@@ -43,54 +47,61 @@ def get_target_contract(date):
     :return:
     """
     sql = f"SELECT contract FROM facTag WHERE tradingday = '{date}'"
+    # print(sql)
     return pd.read_sql_query(sql, conn_tmp)['contract'].tolist()
 
 
 tradeDate = get_trade_day_series(current_date)
+# for date in tradeDate:
+#     target_code = get_target_code(date)
+#     code_str = f"""('{"','".join(target_code)}')"""
+#     sql = f"""
+#              SELECT code,only_letters(code) contract,tradingday,timestamp,closeprice
+#              FROM "bf{date}"
+#              WHERE timestamp>='09:01:00' and timestamp<='14:45:00'
+#              and code in {code_str}
+#            """
+#     data = pd.read_sql_query(sql, conn_his)
+#     print(data)
+#     data.to_sql(f"bf{date}", conn_mydata, if_exists='replace', index=False)
 
-for date in tradeDate:
-    target_code = get_target_code(date)
-    code_str = f"""('{"','".join(target_code)}')"""
-    sql = f"""
-             SELECT code,only_letters(code) contract,tradingday,timestamp,closeprice 
-             FROM "bf{date}" 
-             WHERE timestamp>='09:01:00' and timestamp<='14:45:00'
-             and code in {code_str}
-           """
-    data = pd.read_sql_query(sql, conn_his)
-    print(data)
+# 获取每日交易合约
+print(tradeDate[0])
+target_contract = get_target_contract(tradeDate[0])  # 要把今天需要交易的合约的历史数据找出来
+print(target_contract)
 
-print(1 / 0)
-
-## 获取累计复权因子
-sql_acc_factor = f"SELECT tradingday,prefix,code,accfactor FROM TraderOvk WHERE tradingday = {current_date}"
-
-sql_list = []
-contract_data = []
-tar_contract = ['rb']
-for contract in tar_contract:
-    for date in tradeDate:
-        sql = f"""
-                 SELECT code,only_letters(code) contract,tradingday,timestamp,closeprice 
-                 FROM "bf{date}" 
-                 WHERE timestamp>='09:01:00' and timestamp<='14:45:00'
-                 and contract = '{contract}'
-                 """
-        pd.read_sql_query(sql, conn_his)
-        contract_data.append(pd.read_sql_query(sql, conn_his))
-    data = pd.concat(contract_data)
-    print(data)
-
-print(data)
-print(len(tradeDate))
-print(len(tar_contract))
-print(len(sql_list))
-df = pd.read_sql_query(sql_list[0], conn_his)
-print(df)
-
-print(1 / 0)
+# print(1 / 0)
+#
+# ## 获取累计复权因子
+# sql_acc_factor = f"SELECT tradingday,prefix,code,accfactor FROM TraderOvk WHERE tradingday = {current_date}"
+#
+# sql_list = []
+# contract_data = []
+# tar_contract = ['rb']
+# for contract in tar_contract:
+#     for date in tradeDate:
+#         sql = f"""
+#                  SELECT code,only_letters(code) contract,tradingday,timestamp,closeprice
+#                  FROM "bf{date}"
+#                  WHERE timestamp>='09:01:00' and timestamp<='14:45:00'
+#                  and contract = '{contract}'
+#                  """
+#         pd.read_sql_query(sql, conn_his)
+#         contract_data.append(pd.read_sql_query(sql, conn_his))
+#     data = pd.concat(contract_data)
+#     print(data)
+#
+# print(data)
+# print(len(tradeDate))
+# print(len(tar_contract))
+# print(len(sql_list))
+# df = pd.read_sql_query(sql_list[0], conn_his)
+# print(df)
+#
+# print(1 / 0)
 
 # 6. 关闭连接
 conn_his.close()
 conn_sys.close()
 conn_tmp.close()
+conn_mydata.close()
